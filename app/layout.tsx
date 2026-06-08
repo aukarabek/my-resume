8f39a1339b940999040940013a98cd8683a4e1e6
Karabek <aukarabek@gmail.com>
2026-06-07 21:42:49 -0500

    Changes made on home page

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
           <div className="absolute inset-0 -z-10 bg-black/40" />
 
           {/* Name */}
-          <h1 className="text-5xl font-bold tracking-wide">
+          <h1 className="text-4xl font-bold tracking-wide">
             KARABEK ABIY UULU
           </h1>
 
